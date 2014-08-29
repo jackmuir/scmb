d15a87d05a55e238d5824ba8815bfd076037d43e
jackmuir <jackmuir@Jacks-MacBook-Pro.local>
2014-08-29 18:05:01 +1000

    Fixed hmc

--- src/main/scala/hmc.scala
+++ src/main/scala/hmc.scala
@@ -1,5 +1,7 @@
 import breeze.linalg._
+
 import breeze.stats.distributions.Uniform
+
 import breeze.stats.distributions.Gaussian
 
 class HMC {
@@ -17,26 +19,24 @@ class HMC {
 
   def hmcUpdate(qo: DenseVector[Double], l: Int, ep: Double, acc: Int): (DenseVector[Double], Int) = {
     val uep = 0.9 * ep + 0.2 * uDev.sample() // perturb epsilon pm 10% to avoid cyclic orbits
-    val po = nDev.sample(qo.Length)
-    val ho = calcU(q0) + po dot po
-    val (qn, pn) = leapFrog(qo, po - uep * calcUgrad(qo) / 2.0, l, uep)
-    val hn = calcU(qn) + pn dot pn
+    val po = DenseVector(nDev.sample(qo.length).toArray)
+    val ho = calcU(qo) + (po dot po)
 
-    if (metCheck(ho,hn)) (qn, acc + 1) else (qo, acc)
-
-    def leapFrog(qo: DenseVector[Double], po: DenseVector[Double], l: Int, ep: Dobule) {
+    def leapFrog(qo: DenseVector[Double], po: DenseVector[Double], l: Int, ep: Double): (DenseVector[Double], DenseVector[Double]) = {
       if (l == 1) {
-        val qn = qo + ep * po
-        val pn = qo - ep * calcUgrad(qo) / 2.0
+        val qn = qo + (po :* ep)
+        val pn = qo - (calcUgrad(qo) :* ep) / 2.0
         (qn, pn)
       }
       else {
-        val qn = qo + ep * po
-        val pn = qo - ep * calcUgrad(qo)
+        val qn = qo + (po :* ep)
+        val pn = qo - (calcUgrad(qo) :* ep)
         leapFrog(qn, pn, l - 1, ep)
       }
     }
-}
-
-  val hmcu = (hmcupdate _).tupled
+    
+    val (qn, pn) = leapFrog(qo, po - (calcUgrad(qo) :* uep) / 2.0, l, uep)
+    val hn = calcU(qn) + (pn dot pn)
+    if (metCheck(ho,hn)) (qn, acc + 1) else (qo, acc)
+  }
 }
